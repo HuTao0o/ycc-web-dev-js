677e5899e2fa5086f7cace1c8b88567a7530f69e
hsebk <bekeshchenkoandrey@mail.ru>
2023-01-31 20:01:31 +0300

    31_01 lesson

--- public/site.js
+++ public/site.js
@@ -1,6 +1,8 @@
 let myBtn = document.getElementById('add-btn')
 myBtn.addEventListener('click', addItem)
 
+let countTask = 0
+
 function addItem(data) {
     let myInput = document.getElementById('do-it')
     let myList = document.getElementById('todo-list')
@@ -8,11 +10,11 @@ function addItem(data) {
     if (myInput.value !== '') {
         let newItem = document.createElement('li')
         newItem.classList.add('list-group-item')
-
+        countTask = countTask + 1;
         let newBlock =
             `<div class="form-check">
-                <input class="form-check-input" type = "checkbox" value = "">
-                 <label class="form-check-label" for="ch2">
+                <input onclick="finishTask(${countTask})" class="form-check-input" type = "checkbox" value = "">
+                 <label id="taskName-${countTask}" class="form-check-label" for="ch2">
                     ${myInput.value}
                     </label> 
         </div >`
@@ -25,5 +27,13 @@ document.addEventListener('keydown', keycap)
 function keycap(data) {
     if (data.keyCode == 13) {
         addItem()
+    }
+}
+function finishTask(id) {
+    let taskName = document.getElementById(`taskName-${id}`);
+    if(taskName.classList.contains(`completed-item`)) {
+        taskName.classList.remove(`completed-item`);
+    } else {
+        taskName.classList.add(`completed-item`);
     }
 }
